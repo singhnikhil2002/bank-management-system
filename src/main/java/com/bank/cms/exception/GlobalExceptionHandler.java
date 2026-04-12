e5a8baeb284227cdb668898506d9b187460f6455
Nikhil Singh <singhnikhil2002>
2026-04-12 16:59:26 +0530

    Exception Handling

--- src/main/java/com/bank/cms/exception/GlobalExceptionHandler.java
+++ src/main/java/com/bank/cms/exception/GlobalExceptionHandler.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice(basePackages = "com.bank.cms.controller")
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
--- src/main/java/com/bank/cms/service/impl/TransactionServiceImpl.java
+++ src/main/java/com/bank/cms/service/impl/TransactionServiceImpl.java
@@ -3,12 +3,15 @@ package com.bank.cms.service.impl;
 import com.bank.cms.dto.request.TransactionRequest;
 import com.bank.cms.entity.Account;
 import com.bank.cms.entity.Transaction;
+import com.bank.cms.exception.ResourceNotFoundException;
 import com.bank.cms.repository.AccountRepository;
 import com.bank.cms.repository.TransactionRepository;
 import com.bank.cms.service.TransactionService;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -26,24 +29,24 @@ public class TransactionServiceImpl implements TransactionService {
     public String transferMoney(TransactionRequest request){
         // 1. Fetch Account
         Account sender = accountRepository.findByAccountNumber(request.getFromAccount()).orElseThrow(
-                () -> new RuntimeException("Sender not found")
+                () -> new ResourceNotFoundException("Sender not found")
         );
         Account receiver = accountRepository.findByAccountNumber(request.getToAccount()).orElseThrow(
-                () -> new RuntimeException("Receiver not found")
+                () -> new ResourceNotFoundException("Receiver not found")
         );
 
         if(sender == null || receiver == null){
-            throw new RuntimeException("Invalid Account Number");
+            throw new ResourceNotFoundException("Account cannot be null");
         }
 
         // 2.Same Account check
         if(request.getFromAccount().equals(request.getToAccount())){
-            throw new RuntimeException("Both Accounts are same");
+            throw new ResourceNotFoundException("Both Accounts are same");
         }
 
         // 3. Balance Check
         if(sender.getBalance() < request.getAmount()){
-            throw new RuntimeException("Insufficient Balance");
+            throw new ResourceNotFoundException("Insufficient Balance");
         }
 
         // 4. Debit
@@ -70,8 +73,29 @@ public class TransactionServiceImpl implements TransactionService {
 
     @Override
     public List<Transaction> getTransaction(String fromAccount,String toAccount){
+
+        // 1. Fetch Account
+        Account sender = accountRepository.findByAccountNumber(fromAccount).orElseThrow(
+                () -> new ResourceNotFoundException("Sender not found")
+        );
+        Account receiver = accountRepository.findByAccountNumber(toAccount).orElseThrow(
+                () -> new ResourceNotFoundException("Receiver not found")
+        );
+
+        if(sender == null || receiver == null){
+            throw new ResourceNotFoundException("Account cannot be null");
+        }
+
+        // 2.Same Account check
+        if(sender.equals(receiver)){
+            throw new ResourceNotFoundException("Both Accounts are same");
+        }
+
         List<Transaction> txnDetails = transactionRepository.findByFromAccountOrToAccount(fromAccount,toAccount);
 
+        if(txnDetails.isEmpty()){
+            throw  new ResourceNotFoundException("Invalid account");
+        }
         return txnDetails;
     }
 
